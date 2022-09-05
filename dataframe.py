034f9d684d1d60937ef07cbb6a6c0bd810815f65
jltavera <jl.tavera@uniandes.edu.co>
2022-09-04 22:32:24 -0500

    World Bank Format

--- dataframe.py
+++ dataframe.py
@@ -1,12 +1,20 @@
 #%%
 import pandas as pd
+import numpy as np
 '''
 LOADING DATAFRAMES
 '''
 
-totalPop = pd.read_excel('raw/Total_Population.xls')
+#World Health Organization - WHO
 healthPop = pd.read_csv('raw/Mental_Health.csv')
 whoCodes = pd.read_csv('raw/WHO_codes.csv')
+
+#World Bank
+totalPop = pd.read_excel('raw/Total_Population.xls')
+business = pd.read_csv('raw/New_Business_Registered.csv')
+primarySE = pd.read_csv('raw/Primary_SE.csv')
+secondarySE = pd.read_csv('raw/Secondary_SE.csv')
+young = pd.read_csv('raw/Young.csv')
 
 '''
 WHO COUNTRY CODES FORMAT
@@ -14,16 +22,8 @@ WHO COUNTRY CODES FORMAT
 
 Afg = {'COUNTRY': 'COUNTRY', 'AFG': 'AFG', 'Afghanistan': 'Afghanistan'}
 whoCodes = whoCodes.append(Afg, ignore_index=True)
-whoCodes = whoCodes.rename(columns = {'AFG':'CODE', 'Afghanistan': 'Country'})
-whoCodes = whoCodes.loc[:, ['CODE','Country']]
-
-'''
-TOTAL POPULATION FORMAT
-'''
-
-totalPop = totalPop.loc[:, ['Country Code', '2019']]
-totalPop = totalPop.dropna()
-totalPop = totalPop.rename(columns = { '2019': 'Total Pop','Country Code': 'CODE'})
+whoCodes = whoCodes.rename(columns = {'AFG':'Country Code', 'Afghanistan': 'Country'})
+whoCodes = whoCodes.loc[:, ['Country Code','Country']]
 
 '''
 MENTAL HEALTH SECTOR FORMAT
@@ -40,12 +40,48 @@ healthPop['Mental Health'] = (healthPop['Psychiatrists'] +healthPop['Nurses'] +h
 healthPop = healthPop.loc[:, ['Country', 'Mental Health']]
 
 '''
+WORLD BANK FORMAT
+'''
+#Total Population
+
+totalPop = totalPop.loc[:, ['Country Code', '2019']]
+totalPop = totalPop.dropna()
+totalPop = totalPop.rename(columns = { '2019': 'Total Pop'})
+
+#Business
+
+business = business.replace('..',0.0)
+business = business.drop(['Series Name', 'Series Code', 'Country Name'], axis=1)
+business_cols=[i for i in business.columns if i not in ["Country Code"]]
+for col in business_cols:
+    business[col]=pd.to_numeric(business[col])
+business['Companies'] = business.sum(axis=1, numeric_only= True)
+business = business.loc[:, ['Country Code', 'Companies']]
+
+#School Enrollment
+
+primarySE = primarySE.drop(['Country Name', 'Indicator Name', 'Indicator Code'], axis=1)
+primarySE = primarySE.fillna(0)
+primarySE['primarySE'] = primarySE.max(axis=1)
+primarySE = primarySE.loc[:, ['Country Code', 'primarySE']]
+
+secondarySE = secondarySE.drop(['Country Name', 'Indicator Name', 'Indicator Code'], axis=1)
+secondarySE = secondarySE.fillna(0)
+secondarySE['secondarySE'] = secondarySE.max(axis=1)
+secondarySE = secondarySE.loc[:, ['Country Code', 'secondarySE']]
+
+#Age 0-14
+
+young = young.loc[:, ['Country Code', '2019']]
+young = young.dropna()
+young = young.rename(columns = { '2019': 'Young'})
+
+'''
 MERGE
 '''
 
 whoStats = pd.merge(healthPop, whoCodes)
-data = pd.merge(whoStats, totalPop)
-
+whoData = pd.merge(whoStats, totalPop)
 
 '''
 TOTAL PSYCHOLOGISTS
